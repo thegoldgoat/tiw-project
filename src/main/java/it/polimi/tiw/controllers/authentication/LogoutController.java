e47ad93c04740eec80fa677644a4037ac238356d
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-04-21 10:50:50 +0200

    Logout in HomePage

---
+++ src/main/java/it/polimi/tiw/controllers/authentication/LogoutController.java
@@ -0,0 +1,33 @@
+package it.polimi.tiw.controllers.authentication;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+    String loginPath;
+
+    @Override
+    public void init() throws ServletException {
+        loginPath = getServletContext().getContextPath() + "/login";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        doPost(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        res.sendRedirect(loginPath);
+    }
+}
