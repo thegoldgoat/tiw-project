ddd59a83c82f771a148979b797128c3d81f358c5
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-04-22 15:27:10 +0200

    Example DB files upload, both images and data

---
+++ docs/database_example/SQL/gallery_User.sql
@@ -0,0 +1,5 @@
+insert into gallery.User (UserPk, username, password)
+values (1, 'andrea', '5f3d6952c5c5e22077fabf461de80f1ce475752fe75afcf5ca46bac438405619'),
+       (2, 'jordi', '707d11765de821e5bd52bfc135ce9e1e0400f6aabad78a0cdb4543dc22669e49'),
+       (3, 'mauro', '19e37c4dfe9a2e1570be1f4cbafca76f3ed757231c0865e02465c909d916b1d1'),
+       (4, 'maite', '716cd13cf9d417278e487c5b0e4a24f289102c4d5470fc7825680e59297528a5');
