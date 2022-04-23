4ee1f6ff6459e50962090af0b19d6441760babab
Andrea Somaini <andrea.somaini@mail.polimi.it>
2022-04-23 10:11:00 +0200

    Updated example DB to include email

--- docs/database_example/SQL/gallery_Comment.sql
+++ docs/database_example/SQL/gallery_Comment.sql
@@ -1,5 +1,5 @@
 insert into gallery.Comment (CommentPk, text, username, ImageFk)
-values (1, 'Commento 1 di andrea', 'andrea', 1),
-       (2, 'Commento 2 di andrea', 'andrea', 1),
-       (3, 'Commento 3 di andrea', 'andrea', 2),
-       (4, 'Commento 4 di andrea', 'andrea', 2);
+values  (1, 'Commento 1 di andrea', 'andrea', 1),
+        (2, 'Commento 2 di andrea', 'andrea', 1),
+        (3, 'Commento 3 di andrea', 'andrea', 2),
+        (4, 'Commento 4 di andrea', 'andrea', 2);
